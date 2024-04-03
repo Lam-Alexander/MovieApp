a682c8b90b1d977334c088609c6acd98e1b8645b
Alex <Lam.Alexander@hotmail.com>
2024-04-02 23:53:03 -0600

    Package.json update

--- app/page.js
+++ app/page.js
@@ -2,10 +2,9 @@ import AddPost from "./components/AddPost";
 import PostList from "./components/PostList";
 
 async function getData() {
-  const API_URL = 'https://movie-1gewdwd80-lam-alexanders-projects.vercel.app/api/posts';
-  const res = await fetch(API_URL, { cache: "no-cache" });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+  const res = await fetch('http://localhost:3000/api/posts', {cache: "no-cache"})
+  if(!res.ok) {
+    throw new Error("Failed to fetch data")
   }
   return res.json();
 }
