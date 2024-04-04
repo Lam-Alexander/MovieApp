4d37c4fe2b5eb984c20a21c04f1357ef6cbe5bc2
Alex <Lam.Alexander@hotmail.com>
2024-04-04 16:33:13 -0600

    Change 32

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,47 +1,7 @@
-// import client from "../../libs/prismadb";
-// import { NextResponse } from "next/server";
-
-// // url: http://localhost:3001/api/posts
-
-// export const POST = async (req) => {
-//   try {
-//     const body = await req.json();
-//     const { title, listOfActors, releaseYear } = body;
-//     const newPost = await client.post.create({
-//       data: {
-//         title,
-//         listOfActors,
-//         releaseYear
-//       },
-//     });
-//     return NextResponse.json(newPost);
-//   } catch (error) {
-//     return NextResponse.json(
-//       { message: "Error creating post", error },
-//       { status: 500 }
-//     );
-//   }
-// };
-
-// export const GET = async () => {
-//   try {
-    
-//     const posts = await client.post.findMany({
-//       orderBy: {
-//         createdAt: 'desc', 
-//       },
-//     });
-//     return NextResponse.json(posts); 
-//   } catch (error) {
-    
-//     return NextResponse.json({ message: "Error getting posts", error }, 
-//     { status: 500 });
-//   }
-// };
-
-
 import client from "../../libs/prismadb";
 import { NextResponse } from "next/server";
+
+// url: http://localhost:3001/api/posts
 
 export const POST = async (req) => {
   try {
@@ -56,13 +16,16 @@ export const POST = async (req) => {
     });
     return NextResponse.json(newPost);
   } catch (error) {
-    console.error("Error creating post:", error);
-    return NextResponse.json({ status: 500, message: "Error creating post" });
+    return NextResponse.json(
+      { message: "Error creating post", error },
+      { status: 500 }
+    );
   }
 };
 
 export const GET = async () => {
   try {
+    
     const posts = await client.post.findMany({
       orderBy: {
         createdAt: 'desc', 
@@ -70,8 +33,45 @@ export const GET = async () => {
     });
     return NextResponse.json(posts); 
   } catch (error) {
-    console.error("Error getting posts:", error);
-    return NextResponse.json({ status: 500, message: "Error getting posts" });
+    
+    return NextResponse.json({ message: "Error getting posts", error }, 
+    { status: 500 });
   }
 };
+
+
+// import client from "../../libs/prismadb";
+// import { NextResponse } from "next/server";
+
+// export const POST = async (req) => {
+//   try {
+//     const body = await req.json();
+//     const { title, listOfActors, releaseYear } = body;
+//     const newPost = await client.post.create({
+//       data: {
+//         title,
+//         listOfActors,
+//         releaseYear
+//       },
+//     });
+//     return NextResponse.json(newPost);
+//   } catch (error) {
+//     console.error("Error creating post:", error);
+//     return NextResponse.json({ status: 500, message: "Error creating post" });
+//   }
+// };
+
+// export const GET = async () => {
+//   try {
+//     const posts = await client.post.findMany({
+//       orderBy: {
+//         createdAt: 'desc', 
+//       },
+//     });
+//     return NextResponse.json(posts); 
+//   } catch (error) {
+//     console.error("Error getting posts:", error);
+//     return NextResponse.json({ status: 500, message: "Error getting posts" });
+//   }
+// };
 
