cfcf4b107c3ec3d0a126bd4febd243cfba057dc4
Alex <Lam.Alexander@hotmail.com>
2024-04-04 17:29:19 -0600

    Change 45

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-red-500 to-orange-500 text-white text-center p-4">
-      <div className="container mx-auto">
+      <div className="flex container mx-auto">
         <p>MovieHub</p>
         <p>Contact us at: contact@imr-movies.com</p>
         <p>&copy; 2024 IMR Movies</p>
