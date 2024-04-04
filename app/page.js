0760d016a98da66fe896e07ef41d6cfba7027490
Alex <Lam.Alexander@hotmail.com>
2024-04-04 17:03:06 -0600

    Change 40

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import AddPost from "./components/AddPost";
 import PostList from "./components/PostList";
 
 async function getData() {
-  const res = await fetch(`/api/posts`)
+  const res = await fetch(`/api/posts`, {cache: "no-cache"})
   if(!res.ok) {
     throw new Error("Failed to fetch data")
   }
