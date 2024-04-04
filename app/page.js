a2e98f163bd9401a4319ef85d9d84521850b5663
Alex <Lam.Alexander@hotmail.com>
2024-04-04 16:28:01 -0600

    Change 31

--- app/page.js
+++ app/page.js
@@ -3,31 +3,25 @@ import AddPost from "./components/AddPost";
 import PostList from "./components/PostList";
 
 async function getData() {
-  const res = await fetch('https://movie-alfmfn3j9-lam-alexanders-projects.vercel.app/api/posts', {cache: "no-cache"})
+  const res = await fetch(`/api/posts`, {cache: "no-cache"})
   if(!res.ok) {
     throw new Error("Failed to fetch data")
   }
   return res.json();
 }
 
-
 // async function getData() {
-//   const res = await fetch('http://localhost:3000/api/posts', {cache: "no-cache"})
+//   const res = await fetch('https://movie-alfmfn3j9-lam-alexanders-projects.vercel.app/api/posts', {cache: "no-cache"})
 //   if(!res.ok) {
 //     throw new Error("Failed to fetch data")
 //   }
 //   return res.json();
 // }
 
+
 // async function getData() {
-//   const baseUrl = 'http://localhost:3000/api/posts';
-//   const res = await fetch(baseUrl, {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//   if (!res.ok) {
+//   const res = await fetch('http://localhost:3000/api/posts', {cache: "no-cache"})
+//   if(!res.ok) {
 //     throw new Error("Failed to fetch data")
 //   }
 //   return res.json();
