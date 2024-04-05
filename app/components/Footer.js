87dbffd2528ba2288aca3180783170a36fe1b8ff
Alex <Lam.Alexander@hotmail.com>
2024-04-04 20:46:55 -0600

    Change 49

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-red-700 to-orange-400 text-white text-center ">
       <div className='p-2'>
         <p>MovieHub</p>
-        <p>Contact us at: contact@imr-movies.com</p>
-        <p>&copy; 2024 IMR Movies</p>
+        <p>Contact us at: contact@MovieHub.com</p>
+        <p>&copy; 2024 MovieHub</p>
       </div>
     </footer>
   )
